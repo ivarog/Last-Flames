b9ecd0486a2228a0727ead1ffb62e1c4599e3a44
ivarog <42422542+ivarog@users.noreply.github.com>
2020-03-21 22:45:52 -0600

    Primer commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float velocity;
+    [SerializeField] GameObject gameCamera;
+    // private Rigidbody rb;
+    CharacterController character;
+
+    private void Start() 
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate() 
+    {
+        Move();    
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movement = transform.right * x + transform.forward * z;
+        Vector3 eulerCameraRotation = Camera.main.transform.localRotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(new Vector3(0f, eulerCameraRotation.y, 0f));
+        character.Move(movement * Time.deltaTime * velocity);
+        gameCamera.transform.position = transform.position + new Vector3(0.0f, 0.4f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/PlayerControllerPC.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerPC : MonoBehaviour
+{
+
+    [SerializeField] float velocity;
+    [SerializeField] GameObject gameCamera;
+    // private Rigidbody rb;
+    CharacterController character;
+    [SerializeField] float sensitivity = 100;
+    [SerializeField] Transform playerBody;
+    float xRotation = 0;
+
+    private void Start() 
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate() 
+    {
+        Move();    
+        Rotate();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movement = transform.right * x + transform.forward * z;
+        character.Move(movement * Time.deltaTime * velocity);
+    }
+
+    void Rotate()
+    {
+        float rotateX = Input.GetAxis("Camera Horizontal") * sensitivity * Time.deltaTime;
+        float rotateY = Input.GetAxis("Camera Vertical") * sensitivity * Time.deltaTime;
+
+        xRotation -= rotateY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        gameCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * rotateX);
+    }
+}
