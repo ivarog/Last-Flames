f79d74f2cef4e9061dc64f205eca3b698c5928d8
ivarog <42422542+ivarog@users.noreply.github.com>
2020-04-06 17:39:44 -0500

    Spawn enemigos

--- Assets/Scripts/Bonfire.cs
+++ Assets/Scripts/Bonfire.cs
@@ -2,12 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hoguera : MonoBehaviour
+public class Bonfire : MonoBehaviour
 {
-    [Range(0f, 10f)][SerializeField] float alturaLuz;
+    [Range(0f, 3f)][SerializeField] float intensity;
+    [SerializeField] GameObject lightBonfire;
+    [SerializeField] float timeBonfireLife;
+    [SerializeField] ParticleSystem fire;
+    [SerializeField] ParticleSystem smoke;
+
+    private float speed; 
+    private Light lightBonfireComponent;
+    private bool canReduceLigth;
+    private const float maxIntensity = 3;
+    private float startSizeFire;
+    private float startSizeSmoke;
 
     private void Start() 
     {
+        lightBonfireComponent = lightBonfire.GetComponent<Light>();  
+        startSizeFire = fire.main.startSize.constant;
+        startSizeSmoke = smoke.main.startSize.constant;
+        speed = intensity / timeBonfireLife;
+    }
+
+    private void Update() 
+    {
+        ReduceIntensityBonfire(speed);
+    }
+
+    private void ReduceIntensityBonfire(float speed)
+    {
+        intensity -= speed * Time.deltaTime;
+        ChangeBonfireIntensity(intensity);
+    }
+
+    private void ChangeBonfireIntensity(float intensity)
+    {
+        lightBonfire.transform.position = Vector3.up * intensity;
+        lightBonfireComponent.intensity = intensity;
+        ParticleSystem.MainModule main = fire.main;
+        main.startSize = new ParticleSystem.MinMaxCurve(startSizeFire * intensity / maxIntensity); 
+        main = smoke.main;
+        main.startSize = new ParticleSystem.MinMaxCurve(startSizeSmoke * intensity / maxIntensity); 
 
     }
+
 }
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] GameObject bonfire;
+    [SerializeField] Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bonfire = GameObject.Find("Bonfire");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(bonfire.transform.position);
+        if(Mathf.Abs(Vector3.Distance(transform.position, bonfire.transform.position)) <= 0.5f)
+        {
+            agent.isStopped = true;
+            animator.SetBool("BonfireReached", true);
+            Destroy(gameObject, 8f);
+        }
+    }
+}
