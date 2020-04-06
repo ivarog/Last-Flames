33189ef5274e9fa0fc4c389a4130705314256eb2
ivarog <42422542+ivarog@users.noreply.github.com>
2020-04-05 22:56:29 -0500

    Animaciones armas y higuera se apaga

---
+++ Assets/Scripts/Bonfire.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoguera : MonoBehaviour
+{
+    [Range(0f, 10f)][SerializeField] float alturaLuz;
+
+    private void Start() 
+    {
+
+    }
+}
